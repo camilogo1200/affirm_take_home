da8a5a8ffb8b2bafb02f2e1a2ff4ac32e6d9c8dc
Cristhian Camilo Gómez Narváez <cristhian.gomez@sureswiftcapital.com>
2022-01-18 03:06:43 -0500

    Removed older factories for retrofit services Added Dagger providers for services (Yelp / Places) - HILT

---
+++ app/src/main/java/com/affirm/takehome/data/repositories/network/api/services/PlacesRestaurantService.kt
@@ -0,0 +1,22 @@
+package com.affirm.takehome.data.repositories.network.api.services
+
+import com.affirm.takehome.data.repositories.network.api.dto.PlacesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// Note: You could use this or define your own.
+interface PlacesRestaurantService {
+
+    @GET("nearbysearch/json")
+    suspend fun getPlacesRestaurants(
+        @Query("location") location: String,
+        @Query("pagetoken") pageToken: String,
+        @Query("key") key: String = KEY,
+        @Query("rankby") rankBy: String = "distance",
+        @Query("type") type: String = "restaurant"
+    ): PlacesResponse
+
+    companion object {
+        internal const val KEY = "AIzaSyAcsY9zVrBham7BwJQzRNmtKfOkgtDPZsQ"
+    }
+}
